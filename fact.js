b3a731e5bc1fe4933c1bc443c3c9e212851e6303
U Sen Wan <senwan96@gmail.com>
2022-01-30 12:01:47 +0600

    iterative approach

---
+++ fact.js
@@ -0,0 +1,10 @@
+function isFacorial(n){
+    let answer = 1;
+    for (i=1; i<=n; i++){
+        answer *= i;
+    }
+    return answer;
+}
+let n = 5;
+answer = isFacorial(n);
+console.log(answer);
