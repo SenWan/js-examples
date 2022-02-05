cdc471d4780a3dd23381f532582bc79bcff48bc6
U Sen Wan <senwan96@gmail.com>
2022-02-05 20:13:24 +0600

    happy coding

---
+++ math.js
@@ -0,0 +1,6 @@
+const output = Math.random()
+const secondOutput = Math.random() * 5;
+const rounded = Math.round(secondOutput);
+console.log(output);
+console.log(secondOutput);
+console.log(rounded);
