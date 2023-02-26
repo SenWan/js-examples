0f14460ac99ddcc0b1fe4112ba92f08ae734a036
U Sen Wan <senwan96@gmail.com>
2023-02-26 18:52:59 +0600

    happy coding

---
+++ split.js
@@ -0,0 +1,13 @@
+const lyrics = 'Tumi bondhu kala pakhi ami jeni ki. bonshonto kale tumai bolte pari ni';
+const parts = lyrics.split(' ');
+const sentences = lyrics.split('.');
+const char = lyrics.split('')
+//console.log(char);
+//console.log(sentences);
+//console.log(parts);
+
+const partial = lyrics.slice(5, 8);
+//console.log(partial);
+
+const partial2 = lyrics.substring(5, 8);
+console.log(partial2);
